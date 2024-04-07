6fa056c6871bac4cc7b8007412fca961c43a9bf1
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-04-07 15:48:42 -0300

    :sparkles: feat: Create domain layer for event, attendee and checkIn

---
+++ src/main/java/org/leonardo/passin/domain/checkin/CheckIn.java
@@ -0,0 +1,32 @@
+package org.leonardo.passin.domain.checkin;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.leonardo.passin.domain.attendee.Attendee;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "check_ins")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckIn {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "attendee_id", nullable = false)
+    private Attendee attendeeId;
+
+
+}
