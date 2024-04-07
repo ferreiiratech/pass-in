d5dd2afe64f31d70c1e7b435959aa773f414dda9
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-04-07 14:12:47 -0300

    :tada: first commit

---
+++ src/main/java/org/leonardo/passin/PassInApplication.java
@@ -0,0 +1,13 @@
+package org.leonardo.passin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassInApplication.class, args);
+	}
+
+}
