740d2ff7783a491c3dd3f0ea0ff2d51dc39b06f9
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-04-07 17:49:26 -0300

    :rocket: Implement AttendeeController and AttendeeRepository functionalities

--- src/main/java/org/leonardo/passin/repositories/AttendeeRepository.java
+++ src/main/java/org/leonardo/passin/repositories/AttendeeRepository.java
@@ -3,5 +3,8 @@ package org.leonardo.passin.repositories;
 import org.leonardo.passin.domain.attendee.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+    List<Attendee> findByEventId(String eventId);
 }
