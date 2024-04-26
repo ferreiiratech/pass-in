4bfb33e2154de615f9912d55b10cce6a24142245
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-04-26 17:07:20 -0300

    Create class AttendeeListResponseDTO

--- src/main/java/org/leonardo/passin/dto/attendee/AttendeeListResponseDTO.java
+++ src/main/java/org/leonardo/passin/dto/attendee/AttendeeListResponseDTO.java
@@ -1,2 +1,6 @@
-package org.leonardo.passin.dto.attendee;public class AttendeeListResponseDTO {
+package org.leonardo.passin.dto.attendee;
+
+import java.util.List;
+
+public record AttendeeListResponseDTO(List<AttendeeDetails> attendees) {
 }
