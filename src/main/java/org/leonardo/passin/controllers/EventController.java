7565c85751bbaf985d5ebdeb7189dd0858dfe23f
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-04-26 17:10:46 -0300

    Create class EventController

--- src/main/java/org/leonardo/passin/controllers/EventController.java
+++ src/main/java/org/leonardo/passin/controllers/EventController.java
@@ -1,9 +1,11 @@
 package org.leonardo.passin.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.leonardo.passin.dto.attendee.AttendeeListResponseDTO;
 import org.leonardo.passin.dto.event.EventIdDTO;
 import org.leonardo.passin.dto.event.EventRequestDTO;
 import org.leonardo.passin.dto.event.EventResponseDTO;
+import org.leonardo.passin.services.AttendeeService;
 import org.leonardo.passin.services.EventService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequiredArgsConstructor
 public class EventController {
     private final EventService eventService;
+    private final AttendeeService attendeeService;
 
     @GetMapping("/{eventId}")
     public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String eventId){
@@ -32,4 +35,16 @@ public class EventController {
         return ResponseEntity.created(uri).body(eventIdDTO);
 
     }
+
+
+
+    @GetMapping("/attendees/{eventId}")
+    public ResponseEntity<AttendeeListResponseDTO> getEventAttendees(@PathVariable String eventId){
+        AttendeeListResponseDTO attendeeListResponseDTO = this.attendeeService.getEventAttendees(eventId);
+
+
+        return ResponseEntity.ok(attendeeListResponseDTO);
+    }
+
+
 }
