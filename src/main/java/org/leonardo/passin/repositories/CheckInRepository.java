53b5a3baba95df6241f83c2a22ca5cc731e2dc1d
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-04-26 17:10:10 -0300

    Create repository CheckInRepository class

--- src/main/java/org/leonardo/passin/repositories/CheckInRepository.java
+++ src/main/java/org/leonardo/passin/repositories/CheckInRepository.java
@@ -3,5 +3,8 @@ package org.leonardo.passin.repositories;
 import org.leonardo.passin.domain.checkin.CheckIn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CheckInRepository extends JpaRepository<CheckIn, Integer> {
+    Optional<CheckIn> findByAttendeeId(String attendeeId);
 }
