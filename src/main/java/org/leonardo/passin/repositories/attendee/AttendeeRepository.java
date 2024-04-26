e351902ff2f57139b96075bae54453145eb02583
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2024-04-26 19:52:11 -0300

    Created method for find attendee by email and eventId

--- src/main/java/org/leonardo/passin/repositories/attendee/AttendeeRepository.java
+++ src/main/java/org/leonardo/passin/repositories/attendee/AttendeeRepository.java
@@ -4,7 +4,10 @@ import org.leonardo.passin.domain.attendee.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
     List<Attendee> findAllByEventId(String eventId);
+
+    Optional<Attendee> findByEventIdAndEmail(String email, String eventId);
 }
